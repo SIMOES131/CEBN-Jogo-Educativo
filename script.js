5d5d739489570d47dc37a7bb45134b255c49f841
Oliveira Santos <cammom131@gmail.com>
2025-04-10 09:06:08 -0300

    ajustes

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function jump() {
   pulosRestantes--;
   jumping = true;
 
-  const posicaoBase = 200; // posição original do personagem
+  const posicaoBase = 150; // posição original do personagem
   let jumpHeight = 0;
   const maxJump = isMobile ? 400 : 500;
   const jumpSpeed = isMobile ? 15 : 10;
